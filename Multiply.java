19f89ce99174e455af4a76d141f33123f8c176c5
Kartik Mahendra Tiwari <fybsccssem1@gmail.com>
2021-06-11 15:44:43 +0530

    added Multiply.java to Kartik/multiply

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply {
+	public static void main(String[] args) {
+		int a = 4; 
+		int b = 2;
+
+
+		//Printing the Product of a and B
+		System.out.println("The Product of 2 and 4 is " + (a*b));
+	}
+}
